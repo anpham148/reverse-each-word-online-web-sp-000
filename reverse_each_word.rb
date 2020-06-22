f247dca9e24a6b9e222460ae8a195002040d2101
An Pham <anpham148@gmail.com>
2020-06-22 13:28:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,14 @@
 def reverse_each_word(str)
   arr = str.split
-  arr1 = []
-  arr.each do |word|
-    word1 = word.reverse
-    arr1.push(word1)
+  # arr1 = []
+  # arr.each do |word|
+  #   word1 = word.reverse
+  #   arr1.push(word1)
+  # end
+  arr.collect do |word|
+    word.reversed
   end
-  arr1.join(" ")
+  # arr1.join(" ")
+
+  
 end
